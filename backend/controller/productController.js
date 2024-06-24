7f32a35e3b56b23017b3ad2ac46247d27de8ed69
ayaam.v <ayaam.mit@gmail.com>
2024-06-24 14:35:30 +0530

    getProductDetails route complete

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -11,11 +11,42 @@ exports.createProduct = async (req, res, next) => {
 
 //Get all products
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json({
-    success: true,
-    products,
-  });
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: true,
+      message: "Internal server error",
+    });
+  }
+};
+
+//Get product details
+exports.getProductDetails = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 };
 
 //Update product -- ADMIN
@@ -56,37 +87,35 @@ exports.updateProduct = async (req, res, next) => {
   }
 };
 
-
-//Delete Product 
-exports.deleteProduct = async(req, res, next) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if(!product){
-            return res.status(500).json({
-                success : false, 
-                message : "Product not found"
-            });
-        }
-
-        try{
-            await Product.findByIdAndDelete(req.params.id);
-            res.status(200).json({
-                succces : true,
-                messgae : "Product deleted successfully"
-            });
-        } catch(error) {
-            console.log(error);
-            res.status(500).json({
-                success : false,
-                message : "Failed to delete the product"
-            });
-        }
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success : false,
-            message : "Internal server error"
-        })
+//Delete Product -- ADMIN
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
     }
-}
+
+    try {
+      await Product.findByIdAndDelete(req.params.id);
+      res.status(200).json({
+        succces: true,
+        messgae: "Product deleted successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: "Failed to delete the product",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};

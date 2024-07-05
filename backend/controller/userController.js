d514e375e641be7fd4a36cedd16a7336741940c8
ayaam.v <ayaam.mit@gmail.com>
2024-07-05 16:15:54 +0530

    user CRUD complete

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -28,3 +28,73 @@ exports.registerUser = catchAsyncErrors( async(req, res, next) => {
 //delete a user -- ADMIN
 // get a single user (by id)--ADMIN
 //get all users -- ADMIN
+
+
+//get all users -- ADMIN
+exports.getAllUsers = catchAsyncErrors(async(req, res, next) => {
+
+    const users = await User.find();
+    res.status(200).json({
+        success : true,
+        message : "all users found",
+        users
+    });
+});
+
+
+//get a single user -- ADMIN
+exports.getSingleUser = catchAsyncErrors(async(req, res, next) => {
+    const user = await User.findById(req.params.id);
+    if(!user){
+        res.status(404).json({
+            success : false,
+            message : "user not found"
+        });
+    } else {
+        res.status(200).json({
+            success : true,
+            message : "user found successfully",
+            user
+        });
+    }
+});
+
+
+//update user profile
+exports.updateUser = catchAsyncErrors(async(req, res, next) => {
+    let user = await User.findById(req.params.id);
+    if(!user){
+        res.status(404).json({
+            success : false
+        });
+    }
+
+    user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new : true,
+        runValidators : true,
+    });
+
+    res.status(202).json({
+        success : true,
+        message : "user updated successfully",
+        user,
+    });
+
+});
+
+
+exports.deleteUser = catchAsyncErrors(async(req, res, next) => {
+    let user = await User.findById(req.params.id);
+    if(!user){
+        res.status(404).json({
+            success : false,
+            message : "user not found"
+        });
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        success : true,
+        message : "user deleted successfully"
+    });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const {registerUser} = require("../controller/userController");
+const {registerUser, getAllUsers, getSingleUser, updateUser, deleteUser} = require("../controller/userController");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+router.route("/getAllUsers").get(getAllUsers);
+router.route("/getUser/:id").get(getSingleUser);
+router.route("/updateUser/:id").put(updateUser);
+router.route("/deleteUser/:id").delete(deleteUser);
 
 module.exports = router;

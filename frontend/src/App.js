4aeeee7e5decb2fbe579a33609580b0c5298dac8
ayaam.v <ayaam.mit@gmail.com>
2024-07-12 18:36:42 +0530

    developed header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Header from "./component/layout/Header.js"
+import {BrowserRouter as Router} from "react-router-dom"
+import WebFont from "webfontloader";
+import React from "react";
 
 function App() {
+  React.useEffect(()=> {
+    WebFont.load({
+      google : {
+        families : ["Roboto", "Droid Sans", "Chilank"], 
+      },
+    });
+  }, []);
   return (
-   <Header />
+  <Router>
+    <Header />
+  </Router> 
   );
 }
 

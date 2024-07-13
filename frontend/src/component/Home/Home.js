b4c0af19ca97d8cdc17f64ef4d53db8bf2b4b5d1
ayaam.v <ayaam.mit@gmail.com>
2024-07-13 11:05:04 +0530

    redux set up along with get all products api done

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,44 +1,51 @@
-import React, {Fragment} from 'react'
-import {CgMouse} from "react-icons/cg";
-import "./Home.css"
-import Product from "./Product.js"
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import Product from "./Product.js";
+import MetaData from "../layout/MetaData.js";
+import { getProduct } from "../../actions/productAction.js";
+import { useSelector, useDispatch } from "react-redux";
 
 const product = {
-    name : "Blue Tshirt",
-    images: [{url : "https://i.ibb.co/DRST11n/1.webp"}],
-    price : "$3000",
-    _id:"abhishek"
+  name: "Blue Tshirt",
+  images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+  price: "$3000",
+  _id: "abhishek",
 };
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProduct());
+  }, [dispatch]);
   return (
     <Fragment>
-        <div className = "banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
-            <a href = "#container">
-                <button>
-                    Scroll <CgMouse />
-                </button>
-            </a>
-        </div>
+      <MetaData title="ECOMMERCE" />
+      <div className="banner">
+        <p>Welcome to Ecommerce</p>
+        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+        <a href="#container">
+          <button>
+            Scroll <CgMouse />
+          </button>
+        </a>
+      </div>
 
-        <h2 className='homeHeading'>Featured Products</h2>
+      <h2 className="homeHeading">Featured Products</h2>
 
-        <div className='container' id = "container">
-            <Product product = {product} />
-            <Product product = {product} />
-            <Product product = {product} />
-            <Product product = {product} />
-            <Product product = {product} />
-            <Product product = {product} />
-            <Product product = {product} />
-            <Product product = {product} />
-        </div>
-
-
+      <div className="container" id="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+      </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

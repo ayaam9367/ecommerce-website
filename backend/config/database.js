374c68febb5c0770d5078f808d9b48300a6b59f0
ayaam <ayaam.mit@gmail.com>
2024-07-22 19:20:47 +0530

    database connection secured

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
+  mongoose 
     .connect(process.env.DB_URI, {
      // useNewUrlParser: true,
      // useUnifiedTopology: true,
